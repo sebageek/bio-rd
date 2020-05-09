cc5f90be2eb3e4fde380b7b6c801f4e6492fb0c1
Sebastian Lohff <seba@someserver.de>
2020-05-09 22:21:07 +0200

    Copy ris to multiris

---
+++ cmd/multiris/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"net"
+	"os"
+	"time"
+
+	"google.golang.org/grpc"
+
+	"github.com/bio-routing/bio-rd/cmd/ris/config"
+	"github.com/bio-routing/bio-rd/cmd/ris/risserver"
+	"github.com/bio-routing/bio-rd/protocols/bgp/server"
+	"github.com/bio-routing/bio-rd/util/servicewrapper"
+	"github.com/prometheus/client_golang/prometheus"
+	"google.golang.org/grpc/keepalive"
+
+	pb "github.com/bio-routing/bio-rd/cmd/ris/api"
+	prom_bmp "github.com/bio-routing/bio-rd/metrics/bmp/adapter/prom"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	grpcPort             = flag.Uint("grpc_port", 4321, "gRPC server port")
+	httpPort             = flag.Uint("http_port", 4320, "HTTP server port")
+	grpcKeepaliveMinTime = flag.Uint("grpc_keepalive_min_time", 1, "Minimum time (seconds) for a client to wait between GRPC keepalive pings")
+	configFilePath       = flag.String("config.file", "ris_config.yml", "Configuration file")
+)
+
+func main() {
+	flag.Parse()
+
+	cfg, err := config.LoadConfig(*configFilePath)
+	if err != nil {
+		log.Errorf("Failed to load config: %v", err)
+		os.Exit(1)
+	}
+
+	b := server.NewServer()
+	prometheus.MustRegister(prom_bmp.NewCollector(b))
+
+	for _, r := range cfg.BMPServers {
+		ip := net.ParseIP(r.Address)
+		if ip == nil {
+			log.Errorf("Unable to convert %q to net.IP", r.Address)
+			os.Exit(1)
+		}
+		b.AddRouter(ip, r.Port)
+	}
+
+	s := risserver.NewServer(b)
+	unaryInterceptors := []grpc.UnaryServerInterceptor{}
+	streamInterceptors := []grpc.StreamServerInterceptor{}
+	srv, err := servicewrapper.New(
+		uint16(*grpcPort),
+		servicewrapper.HTTP(uint16(*httpPort)),
+		unaryInterceptors,
+		streamInterceptors,
+		keepalive.EnforcementPolicy{
+			MinTime:             time.Duration(*grpcKeepaliveMinTime) * time.Second,
+			PermitWithoutStream: true,
+		},
+	)
+	if err != nil {
+		log.Errorf("failed to listen: %v", err)
+		os.Exit(1)
+	}
+
+	pb.RegisterRoutingInformationServiceServer(srv.GRPC(), s)
+	if err := srv.Serve(); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+}
